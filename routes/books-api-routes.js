c9fd9096d8e376cf218aa40cc43fe262ef35d511
melissa-stott <1princessmelissa@gmail.com>
2021-03-01 22:04:26 -0800

    added user book list route

--- routes/books-api-routes.js
+++ routes/books-api-routes.js
@@ -1,7 +1,21 @@
 const db = require('../models');
 
 module.exports = (app) => {
-    app.get('/api/books', (req, res) => {
-        db.Book.findAll({}).then((dbBook) => res.json(dbBook));
+    app.get('/api/list:email', (req, res) => {
+        db.Book.findAll({
+            where: {
+                email: req.params.email
+            }
+        }).then((dbBook) => res.json(dbBook));
     });
 }
+
+app.post('/api/books', (req, res) => {
+    console.log(req.body);
+    db.Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        category: req.body.category,
+        email: req.body.email
+    }).then((dbPost) => res.json(dbPost));
+});
