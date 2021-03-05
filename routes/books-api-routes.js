184199e43980664bcb04d479ca455ffc2a3d980a
yvettehenderson <yvette.henderson@gmail.com>
2021-03-05 08:47:52 -0800

    more fetch

--- routes/books-api-routes.js
+++ routes/books-api-routes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     });
 
 
-    app.post('/api/list', (req, res) => {
+    app.post('/api/books', (req, res) => {
         console.log(req.body);
         // res.send('hello')
          db.Book.create({
